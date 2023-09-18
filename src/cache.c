e98193078e8edd20d6cd203410d59a4c62463af9
gary.fly <1779905848@qq.com>
2023-09-18 07:55:22 +0000

    checkTcpBacklogSettings

--- src/cache.c
+++ src/cache.c
@@ -1099,3 +1099,22 @@ void adjustOpenFilesLimit(void) {
     }
   }
 }
+
+void checkTcpBacklogSettings(void) {
+#ifdef HAVA_PROC_SOMAXCONN
+  FILE *fp = fopen("/proc/sys/net/core/somaxconn", "r");
+  char buf[1024];
+  if (!fp) return;
+  if (fgets(buf, sizeof(buf), fp) != NULL) {
+    int somaxconn = atoi(buf);
+    if (somaxconn > 0 && somaxconn < server.tcp_backlog) {
+      cacheLog(
+          CACHE_WARNING,
+          "WARNING: The TCP backlog setting of %d cannot be enforced because "
+          "/proc/sys/net/core/somaxconn is set to the lower value of %d.",
+          server.tcp_backlog, somaxconn);
+    }
+  }
+  fclose(fp);
+#endif
+}
